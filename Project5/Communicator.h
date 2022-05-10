f730f9294a132af122d148ff995efa6c1b1a16ae
Dan <dans344@gmail.com>
2022-05-10 20:28:12 +0300

    updating communicator

--- Project5/Communicator.h
+++ Project5/Communicator.h
@@ -1,5 +1,23 @@
 #pragma once
-class Communicator
-{
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+class Communicator{
+private:
+    ifstream inputFile;
+
+public:
+    //Ctor
+    Communicator(const string fileName);
+    //Dtor
+    virtual ~Communicator();
+    //GETTERS
+
+    const vector<int> &getEdgesData() const;
+
+    vector<int> ReadGraphDetailsFromFile();
+
 };
 
