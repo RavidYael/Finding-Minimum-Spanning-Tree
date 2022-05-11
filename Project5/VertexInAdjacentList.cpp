d62c3107027c319d1842d943d289795cf942a885
Dan <dans344@gmail.com>
2022-05-11 13:02:50 +0300

    Added vertex In adj List class

--- Project5/VertexInAdjacentList.cpp
+++ Project5/VertexInAdjacentList.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "VertexInAdjacentList.h"
+
+const Vertex &VertexInAdjacentList::getVertex() const {
+    return vertex;
+}
+
+int VertexInAdjacentList::getWeight() const {
+    return Weight;
+}
+
+
+VertexInAdjacentList::~VertexInAdjacentList() {
+    //TODO not sure if i need to delete memory allocation of vertex here i think graph will do it.
+}
+
+VertexInAdjacentList::VertexInAdjacentList(Vertex &vertex, int weight) : vertex(vertex), Weight(weight) {}
--- Project5/VertexInAdjacentList.h
+++ Project5/VertexInAdjacentList.h
@@ -4,11 +4,25 @@
 
 #ifndef FINDING_MINIMUM_SPANNING_TREE_VERTEXINADJACENTLIST_H
 #define FINDING_MINIMUM_SPANNING_TREE_VERTEXINADJACENTLIST_H
-
+#include <iostream>
+#include "Vertex.h"
 
 
 class VertexInAdjacentList {
+private:
+    Vertex &vertex;
+    int Weight;
 
+public:
+    //Ctor
+    VertexInAdjacentList(Vertex &vertex, int weight);
+
+    //Dtor
+    virtual ~VertexInAdjacentList();
+
+    const Vertex &getVertex() const;
+
+    int getWeight() const;
 };
 
 
